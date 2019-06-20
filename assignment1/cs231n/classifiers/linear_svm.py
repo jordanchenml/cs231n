6407ff7b48f6973a2e1ab3159db9c4dcdbf7f43c
JordanChen <jordanchenml@gmail.com>
2019-06-20 23:33:03 +0800

    Upload svm & knn.

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -36,14 +36,17 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:, j] += X[i]
+                dW[:, y[i]] += -X[i]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
+    dW /= num_train
 
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
-
+    dW += reg * W
     #############################################################################
     # TODO:                                                                     #
     # Compute the gradient of the loss function and store it dW.                #
@@ -77,8 +80,13 @@ def svm_loss_vectorized(W, X, y, reg):
     # result in loss.                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    scores = X.dot(W)
+    correct_class_scores = scores[range(num_train), list(y)].reshape(-1, 1)
+    margins = np.maximum(0, scores - correct_class_scores + 1)
+    margins[range(num_train), list(y)] = 0
+    loss = np.sum(margins) / num_train + 0.5 * reg * np.sum(W * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -93,7 +101,13 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    coeff_mat = np.zeros((num_train, num_classes))
+    coeff_mat[margins > 0] = 1
+    coeff_mat[range(num_train), list(y)] = 0
+    coeff_mat[range(num_train), list(y)] = -np.sum(coeff_mat, axis=1)
+
+    dW = (X.T).dot(coeff_mat)
+    dW = dW / num_train + reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
